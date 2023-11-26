02b72a888b0aa5bb5a23f2fc3c178b5f9bdd4c58
dalkilicyasin <Yd123456+*>
2023-11-26 20:23:07 +0300

    Initial Commit

---
+++ async-await-githup/async_await_githupApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_await_githupApp.swift
+//  async-await-githup
+//
+//  Created by yasin on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct async_await_githupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
